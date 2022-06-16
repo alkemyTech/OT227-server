2cb9ad84ce80b81276b4cd11587c803498dbc6de
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-06-16 14:46:10 -0300

    added express-validator in auth router

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
+const { body } = require('express-validator');
 
-router.post('/login', UserController.login);
+router.post(
+  '/login',
+  body('email').isEmail(),
+  body('password').not().isEmpty(),
+  UserController.login
+);
 
 module.exports = router;
