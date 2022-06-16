f3c9f4a1d921eae96039fada3257205ae1a319df
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-06-16 12:29:56 -0300

    testimonial model: paranoid:true and edit testimonial  migration

--- migrations/20220616152040-create-testimonial.js
+++ migrations/20220616152040-create-testimonial.js
@@ -6,28 +6,31 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       image: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       content: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: true,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Testimonials');
-  }
+  },
 };
--- models/testimonial.js
+++ models/testimonial.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'Testimonial',
+      paranoid: true,
     }
   );
   return Testimonial;
