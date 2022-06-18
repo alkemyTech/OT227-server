7901098891af433c353cc19849059d27cc41ecc1
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-06-17 23:09:22 -0300

    auth router validations fixes: validationResult used

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/userController');
-const { body } = require('express-validator');
+const { body, validationResult } = require('express-validator');
 
 router.post(
   '/login',
-  body('email').isEmail(),
-  body('password').not().isEmpty(),
+  [
+    body('email').not().isEmpty().isEmail(),
+    body('password').not().isEmpty(),
+    (req, res, next) => {
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        return res.status(400).json(errors);
+      }
+
+      next();
+    },
+  ],
   UserController.login
 );
 
