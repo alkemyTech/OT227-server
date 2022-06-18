9238240b30b467607a3c2da2ae2bf9851d2b5a9d
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-06-17 23:25:30 -0300

    added returns in userController-login to prevent the server from  crashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,11 +6,11 @@ class UserController {
     try {
       const user = await User.findOne({ where: { email: body.email } });
       if (!user) {
-        res.status(400).json({ ok: false });
+        return res.status(400).json({ ok: false });
       }
       const isMatch = await bcrypt.compare(body.password, user.password);
       if (!isMatch) {
-        res.status(400).json({ ok: false });
+        return res.status(400).json({ ok: false });
       }
       res.status(200).json(user);
     } catch (err) {
