fdda5a6b65245b5d7a2c5062987034adead16f89
Oscar Morin <djoscarmorin@gmail.com>
2022-06-19 12:38:19 -0300

    modify line search to database and delete the comments in the code

--- middleware/ownership.js
+++ middleware/ownership.js
@@ -1,10 +1,8 @@
 'use strict';
 const jwt            = require('jsonwebtoken');
 const { response }   = require('express');
-const { sequelize }  = require('../models/index');
-const { QueryTypes } = require('sequelize');
+const { User }  = require('../models/user');
 
-//Verify admin role
 const adminRole = async (req, res = response, next) => {
 
         const token = req.header('x-token');
@@ -17,14 +15,10 @@ const adminRole = async (req, res = response, next) => {
 
         try {
 
-            //Get userID related with the token
             const { userId } = jwt.verify( token, process.env.SECRETORPRIVATEKEY );
-
-            //Search user related with userId
             
-            const users = await sequelize.query('SELECT * FROM Users WHERE id="' + userId + '"', { type: QueryTypes.SELECT });
+            const users = await User.findByPk(2);
 
-            //Verify if the user it was found
             if(!users){
                 return res.status(403).json({
                     msg: 'User not found with userId'
