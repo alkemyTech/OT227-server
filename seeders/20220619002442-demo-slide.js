9151a0d9e5fb9268d2017d405064d75dec05c00f
Paolo Belforte <pbelforte2003@gmail.com>
2022-06-18 21:30:40 -0300

    added slide seeder

---
+++ seeders/20220619002442-demo-slide.js
@@ -0,0 +1,20 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Slide', [{
+      organizationId: 1,
+      text: 'Demo-text',
+      imageUrl: 'https://www.a_random_url.com',
+      order: 1,
+      createdAt: new Date,
+      updatedAt: new Date
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.bulkDelete('Slide', null, {});
+
+  }
+};
