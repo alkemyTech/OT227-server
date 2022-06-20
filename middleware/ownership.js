27f6bc97338e5d0664ad1fa3a7247886bf7add63
Oscar Morin <djoscarmorin@gmail.com>
2022-06-20 16:38:05 -0300

    Delete try/catch and update get token

--- middleware/ownership.js
+++ middleware/ownership.js
@@ -6,40 +6,31 @@ const httpStatus = require('../helpers/httpStatus');
 
 class Role {
 
-  static async adminRole( req, res = response) {
+  static async adminRole( req, res = response) {  
 
-    const token = req.header('x-token');
+    const token = req.headers.authorization.split(" ")[1];
 
-      if ( !token ){
-        return res.status(httpStatus.UNAUTHORIZED).json({
-          msg: 'Not has token in the request'
-        });
-      }
+    if ( !token ){
+      return res.status(httpStatus.UNAUTHORIZED).json({
+        msg: 'Not has token in the request'
+      });
+    }
 
-      const { userId } = jwt.verify( token, process.env.SECRETORPRIVATEKEY );
+    const { id } = jwt.verify( token, process.env.SECRETORPRIVATEKEY );
         
-      try {
+    const users = await User.findByPk( id );
+
+    if(!users){
+      return res.status( httpStatus.FORBIDDEN ).json({
+        msg: 'User not found'
+      } 
+    )}      
+
+    return res.status( httpStatus.OK ).json({
+      msg: 'User authorizaded'
+    });          
       
-        const users = await User.findByPk( userId );
-
-        if(!users){
-          return res.status( httpStatus.FORBIDDEN ).json({
-            msg: 'User not found'
-          } 
-         )}      
-
-        return res.status( httpStatus.OK ).json({
-          msg: 'User authorizaded'
-        });
-            
-      } catch (err) {
-
-        return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-          msg: 'Token not valid'
-        });
-            
-      }
-}
+  }
 }
 
 module.exports = Role;
