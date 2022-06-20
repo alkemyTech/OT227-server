541f17bfa5b168c850f54851830d57e62272a8c2
javicara <javicaraballo1@gmail.com>
2022-06-20 02:30:19 -0300

    Creating Category model, base controller and migration
    Se creo el modelo de la Categoria con los siguientes campos:
    id: INTEGER NOT NULL AUTO_INCREMENT
    name: VARCHAR NOT NULL
    description: VARCHAR NULLABLE
    image: VARCHAR NULLABLE
    timestamps y softDeletes
    debido al soft deltes se puso el paranoid en true

---
+++ controllers/categoryController.js
@@ -0,0 +1,4 @@
+//const { category } = require('../models');
+
+
+module.exports = {}
---
+++ models/category.js
@@ -0,0 +1,33 @@
+//create a sequlize model to represent a category,with an ID not null auto incremental with softdeletes paranoid and timestamps
+module.exports = function(sequelize, DataTypes) {
+  const Category = sequelize.define('Category', {
+      id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+      },
+      name: {
+      type: DataTypes.STRING,
+      allowNull: false
+      },
+      description: {
+      type: DataTypes.STRING,
+      allowNull: true 
+      },
+      image: {
+      type: DataTypes.STRING,
+      allowNull: true
+      },
+      deletedAt: {
+      type: DataTypes.DATE,
+      allowNull: true
+      }
+  }, {
+      paranoid: true,
+      timestamps: true,
+      underscored: true,
+      tableName: 'categories'
+  });
+  return Category;
+  }
