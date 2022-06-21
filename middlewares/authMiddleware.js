ea81e1d5e02e2f917ce1f71c9c919db557f0bcdc
javicara <javiercaraballo97@gmail.com>
2022-06-21 19:25:03 -0300

    resolving the requested changes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,12 +1,24 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-class authenticate extends authExtend {
-    constructor(req, res, next) {
-        const token = req.headers.authorization?.split(' ')[1];
-        const verified = jwt.verify(token, process.env.SECRET_JWT_KEY);
-        req.token = verified.user;
-        next();
+class authenticate {
+  constructor(req, res, next) {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        message: "No token provided",
+      });
     }
+    const verified = jwt.verify(token, process.env.SECRET_JWT_KEY);
+    if (!verified) {
+      return res.status(401).json({
+        message: "Invalid token",
+      });
+    }
+    next();
+  }
 }
 
-module.exports = {authenticate}
+module.exports = {
+  authenticate,
+};
+
