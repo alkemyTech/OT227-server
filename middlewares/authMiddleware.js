a99e939b9b3000051b8bcda7dcb64e446c58ab66
javicara <javiercaraballo97@gmail.com>
2022-06-21 19:25:03 -0300

    resolving the requested changes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,16 +1,12 @@
 const jwt = require('jsonwebtoken')
 
-
-const authenticate = (req, res, next) => {
-
-    const token = req.headers.authorization?.split(' ')[1];
-
-    const verified = jwt.verify(token, process.env.SECRET_JWT_KEY);
-
-    req.token = verified.user;
-
-    next();
-
+class authenticate extends authExtend {
+    constructor(req, res, next) {
+        const token = req.headers.authorization?.split(' ')[1];
+        const verified = jwt.verify(token, process.env.SECRET_JWT_KEY);
+        req.token = verified.user;
+        next();
+    }
 }
 
 module.exports = {authenticate}
--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,9 @@
 var express = require("express");
 var router = express.Router();
-const { createJWTToken } = require("../util/authUtil");
-const bcrypt = require("bcrypt");
-const { authenticate } = require("../middlewares/authMiddleware");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
-
-router.post("/login", async (req, res) => {
-  try {
-    const user = { email: "user@mail.com", password: "userPassword" };
-
-    const token = createJWTToken({ user });
-
-    return res.status(200).json({ token });
-  } catch (err) {
-    return res.status(500).json({ err });
-  }
-});
-
-router.get("/protected", authenticate, async (req, res) => {
-  res.send("protected");
-});
-
 
 module.exports = router;
