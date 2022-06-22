5085aae2d2df883db8a9bff07d691589f35de906
Oscar Morin <djoscarmorin@gmail.com>
2022-06-21 22:51:05 -0300

    Model, migration, seed and controller created

---
+++ migrations/20220622014153-create-organization.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Organizations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      welcomeText: {
+        type: Sequelize.TEXT
+      },
+      aboutUsText: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Organizations');
+  }
+};
---
+++ seeders/20220622014208-create-organizations.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Organizations', [
+    {
+      name: 'Example name Organization', 
+      image: 'https://www.funcion.info/wp-content/uploads/2019/04/funcion-de-la-ong.jpg',
+      address: 'Perez castellano 2121',
+      phone: 84746235,
+      email: 'organization@organization.com',
+      welcomeText: 'Welcome Text',
+      aboutUsText: 'About us text',
+      createdAt: new Date,
+      updatedAt: new Date
+    }], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
