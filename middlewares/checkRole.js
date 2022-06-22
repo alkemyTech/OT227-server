37ccc23e20f5e22584da86f709b631281c3a35be
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-06-22 13:26:54 -0300

    Created constants folder with userRoles and used userRoles in checkRole

--- middlewares/checkRole.js
+++ middlewares/checkRole.js
@@ -1,13 +1,18 @@
 const httpStatus = require('../helpers/httpStatus');
+const UserRoles = require('../constants/userRoles');
+
 class CheckRoleId {
   static async isAdmin(req, res, next) {
     const { authorization } = req.headers;
+
     if (authorization) {
       const [, token] = authorization.split(' ');
       const { roleId } = await jwt.verify(token, process.env.JWT_SECRET);
-      if (roleId === 1) {
+
+      if (roleId === UserRoles.roles.Admin) {
         return next();
       }
+
       return res.status(httpStatus.UNAUTHORIZED).json({
         message: 'You are not authorized to perform this action',
       });
