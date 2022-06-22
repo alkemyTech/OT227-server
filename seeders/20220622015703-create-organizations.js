fe2ffa98cb2e511ff985c539dc511bbc11dba904
Oscar Morin <djoscarmorin@gmail.com>
2022-06-21 23:01:15 -0300

    Example Organization DB

--- seeders/20220622015703-create-organizations.js
+++ seeders/20220622015703-create-organizations.js
@@ -2,15 +2,18 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Organizations', [
+      {
+        name: 'Example name Organization', 
+        image: 'https://www.funcion.info/wp-content/uploads/2019/04/funcion-de-la-ong.jpg',
+        address: 'Perez castellano 2121',
+        phone: 84746235,
+        email: 'organization@organization.com',
+        welcomeText: 'Welcome Text',
+        aboutUsText: 'About us text',
+        createdAt: new Date,
+        updatedAt: new Date
+      }], {});
   },
 
   down: async (queryInterface, Sequelize) => {
