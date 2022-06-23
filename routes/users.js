dda6907d3d9188c8508b2afdb31c2165502bebba
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-06-23 13:33:42 -0300

    Created patch method in routes/users

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 const UserController = require('../controllers/userController');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 router.delete('/:id', UserController.deleteUserById);
+router.patch('/:id', UserController.userUpdate);
 
 module.exports = router;
