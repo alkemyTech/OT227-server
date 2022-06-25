78ccabc3bc48c2bcf56d1ca32fece4a2809a990c
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-06-25 10:59:12 -0300

    validations in routes/users patch

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
 const UserController = require('../controllers/userController');
+const { body } = require('express-validator');
+const Validator = require('../helpers/validator');
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 router.delete('/:id', UserController.deleteUserById);
-router.patch('/:id', UserController.userUpdate);
+router.patch('/:id', [
+  body('firstName').not().isEmpty().isString().optional(),
+  body('lastName').not().isEmpty().isString().optional(),
+  body('email').not().isEmpty().isEmail().optional(),
+  body(
+    'password',
+    'Password must contain at least 8 characters, uppercase, lowercase, number and a symbol'
+  ).isStrongPassword().optional(),
+  Validator.validateFields,
+],UserController.userUpdate);
 
 module.exports = router;
