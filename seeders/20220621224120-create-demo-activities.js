4221100c8f03e95a3c5ffa8d924b4be3151a1c3e
javicara <javiercaraballo97@gmail.com>
2022-06-26 16:05:12 -0300

    resolving the requested changes

--- seeders/20220621224120-create-demo-activities.js
+++ seeders/20220621224120-create-demo-activities.js
@@ -1,30 +1,22 @@
-'use strict';
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-    await queryInterface.bulkInsert('activities', [{
-      name: 'Demo Activity',
-      description: 'This is a demo activity',
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "activities",
+      [
+        {
+          name: "Demo Activity",
+          description: "This is a demo activity",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("activities", null, {});
+  },
 };
