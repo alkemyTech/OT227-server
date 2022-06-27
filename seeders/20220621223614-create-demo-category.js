704e58043d8fb77edb4c5c8898fa9e691a3f51db
javicara <javiercaraballo97@gmail.com>
2022-06-26 22:10:29 -0300

    deleting the '+'

--- seeders/20220621223614-create-demo-category.js
+++ seeders/20220621223614-create-demo-category.js
@@ -16,6 +16,6 @@ module.exports = {
     ]);
   },
   async down(queryInterface, Sequelize) {
-    +(await queryInterface.bulkDelete("Categories", null, {}));
+    await queryInterface.bulkDelete("Categories", null, {});
   },
 };
