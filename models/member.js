9d0349ee41f8de7c4009f0c4059c4e64a5e78e8f
javicara <javicaraballo1@gmail.com>
2022-06-27 20:04:36 -0300

    creating model, migration and seeder of members

---
+++ models/member.js
@@ -0,0 +1,43 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define(
+    "Member",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      facebookUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      instagramUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      linkedinUrl: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {
+      paranoid: true,
+      underscored: true,
+      timestamps: true,
+      tableName: "members",
+    }
+  );
+};
