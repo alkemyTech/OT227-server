b0324bb08e0eda9add917f90ac93adc674739414
Oscar Morin <djoscarmorin@gmail.com>
2022-06-28 12:43:03 -0300

    Service upload image to AWS added

--- services/awsS3.js
+++ services/awsS3.js
@@ -10,18 +10,23 @@ class S3UploadImages {
     });
   }
 
-  async uploadImage(name, image) {
-    // Read content from the file
+  uploadImage(name, image) {
+    
     const fileContent = fs.readFileSync(image);
 
-    // Setting up S3 upload parameters
     const params = {
       Bucket: process.env.AWS_BUCKET,
-      Key: name, // File name you want to save as in S3
+      Key: name, 
       Body: fileContent
     };
 
-    await this.s3.upload(params).promise().then(data => data.Location)
+    return new Promise((resolve, rejects) => {
+     try {
+      this.s3.upload(params).promise().then((data) => {resolve(data.Location)});
+     } catch (error) {
+      rejects(error);
+     }
+    });
   }
 }
 
