d19789b71e32fe80b80858b0f3291f81edba413c
Oscar Morin <djoscarmorin@gmail.com>
2022-07-04 22:28:09 -0300

    Controller and routes of news updated

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -3,19 +3,20 @@ const httpStatus = require('../helpers/httpStatus');
 
 class NewsController {
   static async getById (req, res){
+    let news;
     try {
-     const news = await News.findOne({ where: { id: req.params.id } });
+     news = await News.findOne({ where: { id: req.params.id } });
      if(news){
-      return res.status(httpStatus.OK).json(news);
+      return await res.status(httpStatus.OK).json(news);
      }
-     return res.status(httpStatus.NOT_FOUND).json({
-      msg: 'News not found'
-     });
     } catch (error) {
      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
       msg: error.message
      })
     }
+    return res.status(httpStatus.NOT_FOUND).json({
+        msg: 'News not found'
+       });
  }
 }
 
--- routes/news.js
+++ routes/news.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const { param } = require('express-validator');
+const {validateFields} = require('../helpers/validator');
 const {isAdmin} = require('../middleware/checkRole');
 const {getById} =require('../controllers/newsController');
 
-router.get('/:id',isAdmin,getById);
+router.get('/:id',[
+    isAdmin,
+    param('id').isNumeric(),
+    validateFields
+],getById);
 
 module.exports = router;
