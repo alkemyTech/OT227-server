f1549c9d4da6fbb0d8e22e9abd9f846e77e4c5e8
javicara <javicaraballo1@gmail.com>
2022-07-05 20:39:47 -0300

    adding a validation to fields

--- routes/news.js
+++ routes/news.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
-const { param } = require('express-validator');
+const { param , body} = require('express-validator');
 const {validateFields} = require('../helpers/validator');
 const {isAdmin} = require('../middleware/checkRole');
 const {getById, createNew} =require('../controllers/newsController');
 
-router.post('/',createNew);
+router.post('/', [
+    body('name').not().isEmpty().withMessage('name is required'),
+    body('description').not().isEmpty().withMessage('description is empty'),
+    body('image').not().isEmpty().withMessage('image is required'),
+    body('content').not().isEmpty().withMessage('content is required'),
+    body('categoryId').not().isEmpty().withMessage('categoryId is required'),
+    validateFields
+], createNew);
 
 router.get('/:id',[
     isAdmin,
