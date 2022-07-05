04a23b5e013579cf40a3fd3129eb32f8017394a7
Oscar Morin <djoscarmorin@gmail.com>
2022-07-05 09:36:16 -0300

    Delete news added, controller and route updated

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -18,6 +18,24 @@ class NewsController {
         msg: 'News not found'
        });
  }
+
+ static async deleteById (req, res){
+  const news = await News.findOne({ where: { id: req.params.id } });
+  if(!news){
+    return res.status(httpStatus.NOT_FOUND).json({
+      msg: 'News not found'
+     });
+  }
+    
+  try { 
+     await news.destroy();
+     return res.status(httpStatus.OK).json({ message: 'User deleted succesfully' });
+  } catch (error) {
+   return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+    msg: error.message
+   });
+  }
+}
 }
 
 module.exports = NewsController; 
--- routes/news.js
+++ routes/news.js
@@ -3,12 +3,18 @@ var router = express.Router();
 const { param } = require('express-validator');
 const {validateFields} = require('../helpers/validator');
 const {isAdmin} = require('../middleware/checkRole');
-const {getById} =require('../controllers/newsController');
+const {getById, deleteById} =require('../controllers/newsController');
 
 router.get('/:id',[
     isAdmin,
     param('id').isNumeric(),
     validateFields
 ],getById);
+
+router.delete('/:id',[
+    //isAdmin,
+    param('id').isNumeric(),
+    validateFields
+],deleteById);
 
 module.exports = router;
