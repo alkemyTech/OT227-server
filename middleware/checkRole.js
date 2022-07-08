d8d465d58c725b9d80d517f2b3132ec7ce73e67a
Oscar Morin <djoscarmorin@gmail.com>
2022-07-07 21:58:52 -0300

    Hotfix function isAdmin

--- middleware/checkRole.js
+++ middleware/checkRole.js
@@ -1,5 +1,8 @@
+const jwt = require('jsonwebtoken');
+
 const httpStatus = require('../helpers/httpStatus');
 const UserRoles = require('../constants/userRoles');
+const {User} = require('../models');
 
 class CheckRoleId {
   static async isAdmin(req, res, next) {
@@ -7,7 +10,9 @@ class CheckRoleId {
 
     if (authorization) {
       const [, token] = authorization.split(' ');
-      const { roleId } = await jwt.verify(token, process.env.JWT_SECRET);
+      const {user}  = await jwt.verify(token, process.env.SECRET_JWT_KEY);
+
+      const {roleId} = await User.findOne({ where: { 'email': user } });
 
       if (roleId === UserRoles.roles.Admin) {
         return next();
