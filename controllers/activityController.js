41f111bcebc7aad6b05637fbefae4ccc0c27cd4b
javicara <javicaraballo1@gmail.com>
2022-07-11 20:29:13 -0300

    fixing problem with the model and refactoring the method

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,53 +1,51 @@
-const { Activity } = require('../models');
-const httpStatus = require('../helpers/httpStatus');
+const { Activity } = require("../models");
+const httpStatus = require("../helpers/httpStatus");
 
 class activityController {
+  static async createActivity(req, res) {
+    try {
+      const newActivity = await Activity.create(req.body);
 
-    static async createActivity(req, res) {
+      await newActivity.save();
 
-        try {
-
-            const newActivity = await Activity.create(req.body);
-
-            await newActivity.save();
-
-            return res.status(httpStatus.CREATED).json(newActivity);
-
-        } catch (err) {
-
-            return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: err.message })
-
-        }
-
+      return res.status(httpStatus.CREATED).json(newActivity);
+    } catch (err) {
+      return res
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .json({ message: err.message });
     }
-    
-    static async modifyActiviy(req, res) {
+  }
 
-      const {id} = req.params;
-      const { content, image } = req.body;
+  static async modifyActiviy(req, res) {
+    const { id } = req.params;
+    const { content, image } = req.body;
+
+    if (!content && !image) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        msg: "Not have fields to update",
+      });
+    }
+    const bodyUpdate = {
+      content,
+      image,
+    };
+    let updatedActivity;
+    try {
+      updatedActivity = await Activity.update(bodyUpdate, {
+        where: { id },
+      });
+    } catch (error) {
+      return res
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .json({ msg: error.message });
+    }
+    if (updatedActivity[0]) {
       const activity = await Activity.findOne({ where: { id } });
-
-      if (!activity) {
-          return res.status(httpStatus.NOT_FOUND).json({
-              msg: "Activity not found"
-          });
-          }
-      if (!content && !image) {
-          return res.status(httpStatus.BAD_REQUEST).json({
-              msg: "Not have fields to update"
-          });
-      }
-      const bodyUpdate = {
-          content,
-          image
-      }
-      try {
-          await Activity.update(bodyUpdate, { where: { id } });
-          return res.status(httpStatus.OK).json({ message: "Activity updated" });
-      }
-      catch (error) {
-          return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ msg: error.message });
-      }
+      return res.status(httpStatus.OK).json(activity);
     }
+    return res.status(httpStatus.NOT_FOUND).json({
+      msg: "Activity not found",
+    });
+  }
 }
 module.exports = activityController;
