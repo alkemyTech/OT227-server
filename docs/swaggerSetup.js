7039905fb1a4ddb10b29dc6311f0396dffa682a1
Oscar Morin <djoscarmorin@gmail.com>
2022-07-11 17:17:36 -0300

    Improve and organize the code

--- docs/swaggerSetup.js
+++ docs/swaggerSetup.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const path = require('path');
 
+const {pathCategories, schemaCategories} = require('./pathSchemaCategories');
+
 const swaggerSetup =  { 
   definition :{
     openapi: "3.0.0",
@@ -15,113 +17,7 @@ const swaggerSetup =  {
       },
     ],
     tags : ["Category"],
-    paths: {
-      "/categories":{
-        get: {
-          tags: ["Category"],
-          summary: "Get all categories",
-          responses: {
-            200: {
-              description: "get all categories"
-            },
-            500: {
-              description: "Internal server error"
-            }
-          },
-          security: [
-            {
-              bearerAuth: []
-            }
-          ]
-        },
-        post: {
-          tags: ["Category"],
-          summary: "Create a new category",
-          requestBody: {
-            required: true,
-            content: {
-              "application/json": {
-                schema: {
-                  "$ref": "#/components/schemas/category"
-                }
-              }
-            }
-          },
-          responses: {
-            201: {
-              description: "New category was created"
-            },
-            500: {
-              description: "Internal server error"
-            }
-          }
-        }
-      },
-      "/categories/{id}": {
-        get: {
-          tags: ["Category"],
-          summary: "Get category by id",
-          parameters: [
-            {
-              name: "id",
-              in: "path",
-              description: "Category ID",
-              required: true,
-              schema: {
-                type: "number"
-              }
-            }
-          ],
-          responses: {
-            200: {
-              description: "get category by id"
-            },
-            404: {
-              description: "Category not found"
-            },
-            500: {
-              description: "Internal server error"
-            }
-          },
-          security: [
-            {
-              bearerAuth: []
-            }
-          ]
-        },
-        delete: {
-          tags: ["Category"],
-          summary: "Delete category by id",
-          parameters: [
-            {
-              name: "id",
-              in: "path",
-              description: "Category ID",
-              required: true,
-              schema: {
-                type: "number"
-              }
-            }
-          ],
-          responses: {
-            200: {
-              description: "Delete category by id"
-            },
-            404: {
-              description: "Category not found"
-            },
-            500: {
-              description: "Internal server error"
-            }
-          },
-          security: [
-            {
-              bearerAuth: []
-            }
-          ]
-        }
-      }
-    },
+    paths: pathCategories,
     components: {
       securitySchemes: {
         bearerAuth: {
@@ -130,35 +26,7 @@ const swaggerSetup =  {
           in: "header"
         },
       },
-        schemas: {
-          category: {
-          type: "object",
-          required: ["name"],
-          properties: {
-         id: {
-           type: "number",
-           description: "Autogenerate in the database"
-         },
-         name: {
-            type: "string",
-            description: "Name of category"
-         },
-         image: {
-            type: "string",
-            description: "Image of category"
-         },  
-          description: {
-            type: "string",
-            description: "Description of category"
-          }
-        },
-         example: {
-            name: "Category expample",
-            image: "CategoryExpample.png",
-            description: "Description category expample"
-        },
-      },
-      },
+        schemas: schemaCategories,
     },
   },
   apis: [
