178af5378d4c260835ae618d4aa4f931862f08d8
javicara <javicaraballo1@gmail.com>
2022-07-12 14:13:16 -0300

    creating endpoint to modify and slide as adminstrator

--- controllers/slideController.js
+++ controllers/slideController.js
@@ -1,9 +1,8 @@
-const { Slide } = require('../models');
-const httpStatus = require('../helpers/httpStatus');
+const { Slide } = require("../models");
+const httpStatus = require("../helpers/httpStatus");
 
 class SlideController {
-  
-  static async getById(req,res){
+  static async getById(req, res) {
     try {
       const slide = await Slide.findOne({ where: { id: req.params.id } });
       if (slide) {
@@ -38,23 +37,37 @@ class SlideController {
     }
   }
 
-  static async modifyById(req, res) {
-    const slide = await Slide.findOne({ where: { id: req.params.id } });
-    if (!slide) {
-      return res.status(httpStatus.NOT_FOUND).json({
-        msg: "Slide not found",
+  static async modifySlideById(req, res) {
+    const { id } = req.params;
+    const { organizationId, text, imageUrl, order } = req.body;
+    if (!organizationId && !text && !imageUrl && !order) {
+      return res.status(httpStatus.BAD_REQUEST).json({
+        msg: "Not have fields to update",
       });
     }
+    const bodyUpdate = {
+      organizationId,
+      text,
+      imageUrl,
+      order,
+    };
+    let updatedSlide;
     try {
-      await slide.update(req.body);
-      return res
-        .status(httpStatus.OK)
-        .json({ message: "Slide updated succesfully" });
-    } catch (error) {
-      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        msg: error.message,
+      updatedSlide = await Slide.update(bodyUpdate, {
+        where: { id },
       });
+    } catch (error) {
+      return res
+        .status(httpStatus.INTERNAL_SERVER_ERROR)
+        .json({ msg: error.message });
     }
+    if (updatedSlide[0]) {
+      const slide = await Slide.findOne({ where: { id } });
+      return res.status(httpStatus.OK).json(slide);
+    }
+    return res.status(httpStatus.NOT_FOUND).json({
+      msg: "Slide not found",
+    });
   }
 }
 
