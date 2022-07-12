8eee025e9fb67af97b77d1e98f1be4d0fe249e51
javicara <javicaraballo1@gmail.com>
2022-07-12 13:47:29 -0300

    to amend

--- controllers/slideController.js
+++ controllers/slideController.js
@@ -37,6 +37,25 @@ class SlideController {
       });
     }
   }
+
+  static async modifyById(req, res) {
+    const slide = await Slide.findOne({ where: { id: req.params.id } });
+    if (!slide) {
+      return res.status(httpStatus.NOT_FOUND).json({
+        msg: "Slide not found",
+      });
+    }
+    try {
+      await slide.update(req.body);
+      return res
+        .status(httpStatus.OK)
+        .json({ message: "Slide updated succesfully" });
+    } catch (error) {
+      return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+        msg: error.message,
+      });
+    }
+  }
 }
 
 module.exports = SlideController;
