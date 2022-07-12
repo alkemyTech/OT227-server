4ed1aac3aaab614b4f3d3fe0df2f736d35610eb1
Oscar Morin <djoscarmorin@gmail.com>
2022-07-11 22:37:03 -0300

    Endpoint create member was added

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -11,6 +11,26 @@ class memberController {
             return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: error.message });
         }
     }
+
+    static async register(req,res){
+        const {name, facebookUrl, instagramUrl, linkedinUrl, image, description} = req.body;
+        let member;
+        try {
+         member = await Member.create({
+          name,
+          facebookUrl,
+          instagramUrl,
+          linkedinUrl,
+          image,
+          description
+         });
+        } catch (error) {
+         return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+          msg: error.message
+         });
+        }
+        return res.status(httpStatus.OK).json(member);
+       }
 }
 
 module.exports = memberController;
