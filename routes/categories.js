e7402c683cf748ac2e9422d48c31c41242ac7683
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-07-13 20:45:58 -0300

    fixeo archivo rutas del category

--- routes/categories.js
+++ routes/categories.js
@@ -5,8 +5,8 @@ const CheckRoleId = require('../middleware/checkRole');
 const { param, body, query } = require('express-validator');
 const { validateFields } = require('../helpers/validator');
 
-router.put('/:id', [body('name').not().isEmpty().isString(), body('description').not().isEmpty().isString(), body('image').not().isEmpty().isString(), Validator.validateFields],
-    CheckRoleId.isAdmin, CategoryController.updateCategoryById);
+router.put('/:id', [body('name').not().isEmpty().isString(), body('description').not().isEmpty().isString(), body('image').not().isEmpty().isString(), validateFields],
+    CheckRoleId.isAdmin, categoryController.updateCategoryById);
 router.post('/', [body('name').not().isEmpty().isString(), validateFields], CheckRoleId.isAdmin, categoryController.createCategory);
 router.get('/',[query('page').not().isEmpty().isInt({min:1}).optional(),CheckRoleId.isAdmin,
 validateFields],categoryController.getAllCategories);
