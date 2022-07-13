e05ecefd999d4e085b63aab30b5f55483112c63c
Gabriel Isaías González <gabriel.gonzalez.unt@gmail.com>
2022-07-13 19:37:21 -0300

    Admin control

--- routes/slides.js
+++ routes/slides.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const SlideController = require('../controllers/slideController');
+const CheckRoleId = require('../middleware/checkRole');
 
-router.get('/', SlideController.getAllSlides);
+router.get('/', CheckRoleId.isAdmin, SlideController.getAllSlides);
 
 module.exports = router
